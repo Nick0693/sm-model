6d751bad325f9aadf0c2042efb0eabbb47723462
Nick0693 <73828357+Nick0693@users.noreply.github.com>
2021-10-06 15:17:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bathy_merge",
+    # description="Bathymetry utilities",
+    # author="Nicklas Simonsen",
+    # author_email="nisi@dhigroup.com",
+    packages=find_packages(),
+    include_package_data=True,
+)
